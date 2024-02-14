49b8b782a1e7df9628dfd2de7f9de82e4a2ce273
Lee Griever <leegriever@Lees-MacBook-Pro.local>
2024-02-15 01:14:27 +0200

    integrate codemirror and client side web socket

--- client/src/CodeBlock.js
+++ client/src/CodeBlock.js
@@ -1,49 +1,116 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect, useState, useRef} from 'react'
 import {useNavigate, useParams} from "react-router-dom";
 import axios from 'axios';
 import {Button} from '@mui/material';
-import Editor from "@monaco-editor/react"
+
+import CodeMirror from 'codemirror';
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/dracula.css';
+import 'codemirror/theme/darcula.css';
+import 'codemirror/theme/nord.css';
+
+import 'codemirror/mode/javascript/javascript';
+import 'codemirror/addon/edit/closetag';
+import 'codemirror/addon/edit/closebrackets';
+
+import {initSocket} from './socket';
 
 function CodeBlock() {
 
-const {blockId} = useParams();
-const navigate = useNavigate();
+  const socketRef = useRef(null);
+  const codeRef = useRef(null);
+  const {blockId} = useParams();
+  const navigate = useNavigate();
+  const editorRef = useRef(null);
+  const roomId = 1;
 
-const [block, setBlock] = useState([]);
+  const [block, setBlock] = useState([]);
 
-const baseURL = "http://localhost:3080";
+  const baseURL = "http://localhost:3080";
 
-useEffect(() => {
-  getBlock();
-})
 
-const getBlock = () => {
-  axios.get(`${baseURL}/blocks/${blockId}`)
-      .then((response) => setBlock(response.data.block))
-      .catch((error) => console.error(error));
-}
+  useEffect(() => {
+    // init socket
+    const init = async () => {
+      socketRef.current = await initSocket();
+      socketRef.current.on('connect_error', (err) => handleErrors(err));
+      socketRef.current.on('connect_failed', (err) => handleErrors(err));
 
-const onLobbyBtn = () => {
-    navigate('/')
-}
+      function handleErrors(e) {
+          console.log('socket error', e);
+          navigate('/');
+      }
 
-  return (
-    <div>
-      <h1>Code Block</h1>
-      <Editor
-        height = "700px"
-        width = "700px"
-        theme = "vs-dark"
-        defaultLanguage = "javascript"
-      />
-      <Button variant="contained" disableElevation
-      onClick={() => onLobbyBtn(block.id)}
-      >
-      Back To Lobby
-      </Button>
-              
-    </div>
-  )
+      socketRef.current.emit('join', {
+          roomId,
+      });
+    }
+    init();
+  }, [navigate]);
+
+  useEffect(() => {
+    getBlock();
+  });
+
+  useEffect(() => {
+    // Initialize CodeMirror instance
+      const textarea = document.getElementById('realtimeEditor');
+      editorRef.current = CodeMirror.fromTextArea(textarea, {
+          mode: 'javascript',
+          theme: 'nord',
+          autoCloseTags: true,
+          autoCloseBrackets: true,
+          lineNumbers: true,
+      });
+
+
+      // Handle local code changes and emit to other clients
+      const handleCodeChange = (instance, changes) => {
+        const {origin} = changes;
+        const code = instance.getValue();
+        console.log(code)
+
+        codeRef.current = code;
+
+        // Emit the code change to other clients in the same room
+        if (origin !== 'setValue' && socketRef.current) {
+            socketRef.current.emit('code-change', {
+                roomId,
+                code,
+            });
+        }
+    };
+    editorRef.current.on('change', handleCodeChange);
+
+    return () => {
+      editorRef.current.off('change', handleCodeChange);
+      editorRef.current.toTextArea(); // Clean up the CodeMirror instance
+    };
+    
+  });
+
+  const getBlock = () => {
+    axios.get(`${baseURL}/blocks/${blockId}`)
+        .then((response) => setBlock(response.data.block))
+        .catch((error) => console.error(error));
+  }
+
+  const onLobbyBtn = () => {
+      navigate('/')
+  }
+
+    return (
+      <div>
+        <h1>Code Block</h1>
+        <textarea id="realtimeEditor"></textarea>
+        <Button variant="contained" disableElevation
+        onClick={() => onLobbyBtn(block.id)}
+        >
+        Back To Lobby
+        </Button>
+                
+      </div>
+    )
 }
 
 export default CodeBlock
